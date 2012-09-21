cc59934471bcca27539680ed015f25e48675db14
Marten Klitzke <m.klitzke@gmail.com>
2012-09-21 23:30:32 +0200

    locales

--- lib/ogone-rails.rb
+++ lib/ogone-rails.rb
@@ -9,11 +9,4 @@ require 'ogone-rails/helpers'
 require 'ogone-rails/check-auth'
 
 module OgoneRails
-  class Railtie < ::Rails::Railtie
-    config.after_initialize do
-      # Add load paths straight to I18n, so engines and application can overwrite it.
-      require 'active_support/i18n'
-      I18n.load_path += Dir[File.expand_path('../locales/*.yml', __FILE__)]
-    end
-  end
 end
